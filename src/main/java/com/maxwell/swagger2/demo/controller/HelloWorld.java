37776801d0f3ca63b0973251d8565d46a6b91b3f
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2020-06-27 15:06:43 -0700

    adding default logger

--- src/main/java/com/maxwell/swagger2/demo/controller/HelloWorld.java
+++ src/main/java/com/maxwell/swagger2/demo/controller/HelloWorld.java
@@ -1,13 +1,18 @@
 package com.maxwell.swagger2.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 	
+	Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
 	@GetMapping(value="/sayHello")
 	public String sayHelloWorld() {
+		logger.debug("Invoking Hello World");
 		return "HelloWorld\n";
 	}
 }
